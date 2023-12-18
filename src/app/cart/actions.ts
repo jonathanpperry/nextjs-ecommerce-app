4fd68ab2e6f957f35482a5ff0764e92bbfe8c38a
Jonathan Perry <jpper@umich.edu>
2023-12-17 20:34:07 -0600

    Implement prisma extension to update the updatedAt timestamp on cart updates

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -2,7 +2,7 @@
 
 import { createCart, getCart } from '@/lib/db/cart';
 import { revalidatePath } from 'next/cache';
-import prisma from '@/lib/db/prisma';
+import { prisma } from '@/lib/db/prisma';
 
 export async function setProductQuantity(productId: string, quantity: number) {
   const cart = (await getCart()) ?? (await createCart());
@@ -11,22 +11,40 @@ export async function setProductQuantity(productId: string, quantity: number) {
 
   if (quantity === 0) {
     if (articleInCart) {
-      await prisma.cartItem.delete({
-        where: { id: articleInCart.id },
+      await prisma.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: {
+            delete: { id: articleInCart.id },
+          },
+        },
       });
     }
   } else {
     if (articleInCart) {
-      await prisma.cartItem.update({
-        where: { id: articleInCart.id },
-        data: { quantity },
+      await prisma.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: {
+            update: {
+              where: {
+                id: articleInCart.id,
+              },
+              data: { quantity },
+            },
+          },
+        },
       });
     } else {
-      await prisma.cartItem.create({
+      await prisma.cart.update({
+        where: { id: cart.id },
         data: {
-          cartId: cart.id,
-          productId,
-          quantity,
+          items: {
+            create: {
+              productId,
+              quantity,
+            },
+          },
         },
       });
     }
--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,6 +1,6 @@
 import { Cart, CartItem, Prisma } from '@prisma/client';
 import { cookies } from 'next/dist/client/components/headers';
-import prisma from './prisma';
+import { prisma } from './prisma';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../authOptions';
 
@@ -104,12 +104,18 @@ export async function mergeAnonymousCartIntoUserCart(userId: string) {
         where: { cartId: userCart.id },
       });
 
-      await tx.cartItem.createMany({
-        data: mergedCartItems.map((item) => ({
-          cartId: userCart.id,
-          productId: item.productId,
-          quantity: item.quantity,
-        })),
+      await tx.cart.update({
+        where: { id: userCart.id },
+        data: {
+          items: {
+            createMany: {
+              data: mergedCartItems.map((item) => ({
+                productId: item.productId,
+                quantity: item.quantity,
+              })),
+            },
+          },
+        },
       });
     } else {
       await tx.cart.create({
